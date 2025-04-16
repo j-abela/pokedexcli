f14f8365b8fb23a9b642a20b0fe6480cd8d5f43b
j-abela <jacobabela@gmail.com>
2025-04-17 01:35:54 +0800

    implemented user input and outputs first cleaned word

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	input := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		input.Scan()
+		inputText := input.Text()
+		words := cleanInput(inputText)
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
