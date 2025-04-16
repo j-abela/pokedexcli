94a95d6c9afad345abad8800dd55e49291f6dc4b
j-abela <jacobabela@gmail.com>
2025-04-17 02:01:12 +0800

    implemented commands map for help and exit, plus callback functions

--- repl.go
+++ repl.go
@@ -19,11 +19,24 @@ func startRepl() {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
 		inputText := reader.Text()
+
 		words := cleanInput(inputText)
 		if len(words) == 0 {
 			continue
 		}
+
 		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
