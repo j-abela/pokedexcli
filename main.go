c08506a47977f3189bb886dccb0a57da6d361edc
j-abela <jacobabela@gmail.com>
2025-04-17 01:41:43 +0800

    refactored repl logic into startRepl function, called this function from main()

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		reader.Scan()
-		inputText := reader.Text()
-		words := cleanInput(inputText)
-		if len(words) == 0 {
-			continue
-		}
-		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
-	}
+	startRepl()
 }
